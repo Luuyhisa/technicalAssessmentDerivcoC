1784118b18a15457b2e2106b7a357d05051b2071
Luuyhisa <216060117@mycput.ac.za>
2022-09-14 21:09:37 +0200

    Finale

--- Program.cs
+++ Program.cs
@@ -22,9 +22,7 @@ namespace _8
             var dict = new Dictionary<string, int> { };
             using (var sr = new StreamReader(filenamer))
             {
-                //  Console.WriteLine(sr.ReadLine());
-
-                string line;
+                string line, name;
                 List<string> scoreResults = new List<string>();
                 List<string> nameResults = new List<string>();
                 while ((line = sr.ReadLine()) != null)
@@ -34,33 +32,26 @@ namespace _8
                         int charLocation = line.IndexOf(':', StringComparison.Ordinal);
                         if (charLocation > 0)
                         {
-                            string name = line.Substring(0, charLocation);
+                             name = line.Substring(0, charLocation);
                             int intFirstCard = charLocation - 3;
-
-
                             string Card1 = line.Substring(charLocation + 1, 2);
                             List<char> Card1List = new List<char>();
                             Card1List.AddRange(Card1);
-
                             string Card2 = line.Substring(charLocation + 4, 2);
                             List<char> Card2List = new List<char>();
                             Card2List.AddRange(Card2);
-
                             string Card3 = line.Substring(charLocation + 7, 2);
                             List<char> Card3List = new List<char>();
                             Card3List.AddRange(Card3);
-
                             string Card4 = line.Substring(charLocation + 10, 2);
                             List<char> Card4List = new List<char>();
                             Card4List.AddRange(Card4);
-
                             string Card5 = line.Substring(charLocation + 13, 2);
                             List<char> Card5List = new List<char>();
                             Card5List.AddRange(Card5);
                             int tot = calc(Card1List[0].ToString()) + calc(Card2List[0].ToString()) + calc(Card3List[0].ToString()) + calc(Card4List[0].ToString()) + calc(Card5List[0].ToString());
                             totSuit = calcSuit(Card1List[1].ToString()) + calcSuit(Card2List[1].ToString()) + calcSuit(Card3List[1].ToString()) + calcSuit(Card4List[1].ToString()) + calcSuit(Card5List[1].ToString());
                             dict.Add(name + "#" + totSuit, tot);
-
                         }
                         else
                         {
@@ -77,13 +68,9 @@ namespace _8
             {
                 if (dict.Values.Max() == kvp.Value)
                 {
-                    suitDict.Add(kvp.Key.Split("#")[0], Int32.Parse(kvp.Key.Split("#")[1]));
+                    suitDict.Add(kvp.Key.Split("#")[0], Int32.Parse(kvp.Key.Split("#")[1]));                              
                 }
-
             }
-
-
-
             foreach (KeyValuePair<string, int> kvp in suitDict)
             {
                 if (suitDict.Values.Max() == kvp.Value)
@@ -97,44 +84,38 @@ namespace _8
                     {
                         winner.Append("," + kvp.Key.Split("#")[0]);
                     }
+                    Console.WriteLine(winner);
+
+
                     int intWinners = winner.ToString().LastIndexOf(winner.ToString());
                     string stringWinner = winner.ToString().Substring(0, winner.ToString().Length - intWinners);
+                                                Console.WriteLine(stringWinner);
+
                     finctionWrite(stringWinner, dict.Values.Max());
                 }
             }
-            void finctionWrite(string name, int score)
+            void finctionWrite(string namer, int score)
             {
                 try
                 {
                     string filename = Path.Combine(strPath, "abc.txt");
                     File.Delete(filename);
                     StreamWriter sw = new StreamWriter(filename, true, Encoding.ASCII);
-                    if ("Error".Equals(name))
+                    if ("ERROR".Equals(namer))
                     {
-                        sw.Write(name);
+                        sw.Write(namer);
                     }
                     else
                     {
-                        sw.Write(name = " : " + score);
+                        sw.Write(namer + " : " + score);
                     }
                     sw.Close();
-
-
-
-
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e);
                 }
                 finally { }
-
-
-
-
-
-
-
             }
 
             int calc(string CardNumber)
@@ -145,8 +126,6 @@ namespace _8
                 }
                 else
                 {
-
-
                     switch (CardNumber)
                     {
                         case "A":
@@ -175,8 +154,6 @@ namespace _8
                 }
                 else
                 {
-
-
                     switch (CardGroup)
                     {
                         case "H":
@@ -193,12 +170,8 @@ namespace _8
                             break;
                     }
                 }
-
-
                 return numVal1;
             }
-
-
         }
     }
 }
